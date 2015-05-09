bd91fb3c3d719d6436ef01793345d5daccc2c1f2
XanMead <x.m.mead@gmail.com>
2015-05-08 23:11:58 -0400

    implemented phase transitions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
 	populateCache();
-	newGame();
+	resetGame();
 
 	// Start button handler
-	$('#start').on('click', function() {startGame();})
+	$('#start').on('click', function() {startGame();});
+
+	// Option click handler
+	$('.option').on('click', function(event) {registerChoice(event);});
+
+	// Response submit click handler
+	$('#submit').on('click', function() {logResponse();});
+
+	// New game button
+	$('#again').on('click', function() {resetGame();});
 });
 
 function Question(questionText, optRight, opt1, opt2, opt3, picName) {
@@ -20,56 +29,37 @@ function Question(questionText, optRight, opt1, opt2, opt3, picName) {
 	
 	// Index of the correct answer in the options array.
 	this.correctAnswer = 0;
+	this.chosenAnswer = -1;
+	this.isCorrect = false;
 	
-	// Randomizes the order of the available options.
-	this.shuffleAnswers = function() {
+	this.logAnswer = function(answer) {
+		this.chosenAnswer = answer;
+		this.isCorrect = (answer == this.correctAnswer);
+		return this.isCorrect;
+	};
+
+	// Randomizes the order of the available options and sets chosenAnswer to -1.
+	this.reset = function() {
 		var answer = this.options[this.correctAnswer];
 		shuffle(this.options);
 		this.correctAnswer = this.options.indexOf(answer);
+		this.chosenAnswer = -1;
 	}
 }
 
 /* Collection of all the questions a game could choose. */
 var questionCache = new Array();
 
-/* Construct questions here and add them to the cache. */
-function populateCache() {
-	questionCache.push(new Question("Which of these items was not cause for the Moon God Tsukiyomi to murder Uke Mochi, the Goddess of Food?",
-		"Azuki Beans",
-		"Game",
-		"Rice",
-		"Fish",
-		"uke_mochi.jpg"));
-	questionCache.push(new Question("Which Shinto text came first?",
-		"Kojiki",
-		"Nihongi",
-		"Yengishiki",
-		"Kogoshui",
-		"text.jpg"));
-	questionCache.push(new Question("Japanese students hoping to pass their exams often pray to...",
-		"Tenjin",
-		"Omoikane",
-		"Hachiman",
-		"Amaterasu",
-		"wishes.jpg"));
-	questionCache.push(new Question("Hikikomori (shut-ins) in Japan could identify with the deity...",
-		"Amaterasu",
-		"Ryūjin",
-		"Susano-o",
-		"Uzume",
-		"hikikomori.jpg"));
-	questionCache.push(new Question("Which of these is not among the three Imperial Regalia of Japan?",
-		"Tonbogiri, the Dragonfly",
-		"Kusanagi, the Sword",
-		"Yata no Kagami, the Mirror",
-		"Yasakani no Magatama, the Jewel",
-		"kyoko.jpg"));
-	questionCache.push(new Question("Which yōkai (supernatural creature) is known for its prodigious testicles and fondness for shapeshifting?",
-		"Tanuki",
-		"Kitsune",
-		"Kawauso",
-		"Mujina"));
-}
+/* Range of responses to different quiz results. */
+
+var feedback = [
+	"Oof. Study up! Tenjin does not approve.",
+	"One is better than none. Keep trying.",
+	"Try again! You're allowed to look things up.",
+	"More than half! You're alright. Keep trying.",
+	"So close. Try again, you might do better this time.",
+	"100%! Well done! Play again for different questions."
+	];
 
 /* The current question the quiz is on. */
 var qNumber;
@@ -77,19 +67,78 @@ var qNumber;
 /* Array of the questions to be presented in this game. */
 var gameQuestions;
 
+/* Index of the option that's currently selected. */
+var selectedAnswer;
+
+/* Number of questions answered correctly. */
+var numCorrect;
+
+/* Construct questions here and add them to the cache. */
+function populateCache() {
+	questionCache.push(new Question(
+		"Which of these items was not cause for the Moon God Tsukiyomi to murder Uke Mochi, the Goddess of Food?",
+		"Azuki Beans",
+		"Game",
+		"Rice",
+		"Fish",
+		"uke_mochi.jpg"));
+	questionCache.push(new Question(
+		"Which Shinto text came first?",
+		"Kojiki",
+		"Nihongi",
+		"Yengishiki",
+		"Kogoshui",
+		"text.jpg"));
+	questionCache.push(new Question(
+		"Japanese students hoping to pass their exams often pray to...",
+		"Tenjin",
+		"Omoikane",
+		"Hachiman",
+		"Amaterasu",
+		"wishes.jpg"));
+	questionCache.push(new Question(
+		"Hikikomori (shut-ins) in Japan could identify with the deity...",
+		"Amaterasu",
+		"Ryūjin",
+		"Susano-o",
+		"Uzume",
+		"hikikomori.jpg"));
+	questionCache.push(new Question(
+		"Which of these is not among the three Imperial Regalia of Japan?",
+		"Tonbogiri, the Dragonfly",
+		"Kusanagi, the Sword",
+		"Yata no Kagami, the Mirror",
+		"Yasakani no Magatama, the Jewel",
+		"kokyo.jpg"));
+	questionCache.push(new Question(
+		"Which of these yokai (supernatural creature) is known for its prodigious testicles and fondness for shapeshifting?",
+		"Tanuki",
+		"Kitsune",
+		"Kawauso",
+		"Mujina"));
+	questionCache.push(new Question(
+		"The Sessho-seki (Killing Stone) was said to have been haunted by the spirit of a...",
+		"Nine-Tailed Fox",
+		"Saber-Wielding Badger",
+		"Tatsu (Dragon)",
+		"Burning Wheel",
+		"sessho-seki.jpg"));
+}
+
 function chooseGameQuestions() {
 	gameQuestions = new Array();
 	shuffle(questionCache);
 	for (var i = 0; i < 5; i++) {
 		console.log(questionCache[i]);
 		gameQuestions[i] = questionCache[i];
-		gameQuestions[i].shuffleAnswers();
+		gameQuestions[i].reset();
 	}
 }
 
-function newGame() {
+function resetGame() {
 	chooseGameQuestions();
 	qNumber = 0;
+	numCorrect = 0;
 
 	resetBlossoms();
 
@@ -112,7 +161,7 @@ function startGame() {
 /* q must be a Question object */
 function propagateQuestion(q) {
 	// increment question number
-	$('#q-number').text(++qNumber);
+	$('#q-number').text(qNumber + 1);
 	
 	// set question text
 	$('#question-text').text(q.questionText);
@@ -122,8 +171,49 @@ function propagateQuestion(q) {
 	
 	// set question options
 	for (var i = 0; i < 4; i++) {
-		$('#opt-' + i).text(q.options[i]);
+		$('#opt' + i).text(q.options[i]);
 	}
+	selectedAnswer = -1;
+}
+
+/* Sets selectedAnswer to option index.
+ * Sets selected class on clicked element */
+function registerChoice(event) {
+	// unselect the last selection
+	$('#opt' + selectedAnswer).removeClass('selected');
+	var choice = $(event.target);
+	// parse which option was chosen
+	selectedAnswer = parseInt(choice.attr('data-opt'));
+	choice.addClass('selected');
+	console.log(selectedAnswer);
+}
+
+/* Set chosenAnswer on the current question and
+ * move on to the next question. */
+function logResponse() {
+	$('#opt' + selectedAnswer).removeClass('selected');
+	var result = gameQuestions[qNumber].logAnswer(selectedAnswer);
+	numCorrect += result;
+	setPetal(qNumber, result);
+	if (qNumber < 4) {
+		qNumber++;
+		propagateQuestion(gameQuestions[qNumber]);
+	}
+	else {
+		finishGame();
+	}
+}
+
+function finishGame() {
+	$('#question-box').hide();
+	$('.status-box').hide();
+	$('#end-box').show();
+	$('#num-correct').text(numCorrect);
+	setFeedback();
+}
+
+function setFeedback() {
+	$('#feedback').text(feedback[numCorrect]);
 }
 
 /* petal: which petal to set (0 - 4)
@@ -131,12 +221,12 @@ function propagateQuestion(q) {
 function setPetal(petal, shade) {
 	var url = "images/no-p.png";
 	if (shade == 1) {
-		url = "images/bright-p.png";
+		url = "images/light-p.png";
 	}
 	else if (shade == 0) {
 		url = "images/dark-p.png";
 	}
-	$('#p-' + petal).attr('src', url);
+	$('.p' + petal).attr('src', url);
 }
 
 /* Sets blossoms to blank. */
